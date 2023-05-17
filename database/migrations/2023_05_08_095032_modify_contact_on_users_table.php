8fa1080d5f3ff466a7a87892f92e680479a5f8b4
Brucenigel <brucenige.vilo@sun-asterisk.com>
2023-05-17 11:00:36 +0800

    modidy contact

--- database/migrations/2023_05_08_095032_modify_contact_on_users_table.php
+++ database/migrations/2023_05_08_095032_modify_contact_on_users_table.php
@@ -10,14 +10,8 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('address');
-            $table->integer('contact');
-            $table->date('birthdate');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('contact')->change();
         });
     }
 
@@ -26,6 +20,8 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('contact')->change();
+        });
     }
 };
